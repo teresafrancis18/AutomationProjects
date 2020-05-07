a78db028e56fa665f18241558d3e10eb2648750e
Teresa <teresafrancis18@gmail.com>
2020-05-07 14:04:19 -0400

    Updated on 05/07/2020

--- JustEatAutomation/src/test/java/automationscripts/JustEatAutomation/Automation.java
+++ JustEatAutomation/src/test/java/automationscripts/JustEatAutomation/Automation.java
@@ -1,9 +1,13 @@
 package automationscripts.JustEatAutomation;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -16,11 +20,14 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import com.deque.axe.AXE;
+
 
 public class Automation 
 {
 	//Driver variable is declared as a global variable
 	WebDriver driver;
+	URL axe_Script=Automation.class.getResource("axe.min.js");
 	
 	//Setup the browser for test
 	@BeforeTest
@@ -31,11 +38,41 @@ public class Automation
 	driver.manage().window().maximize();
 	}
 	
-	@Test//Test for checking the application launch
+	@Test//Test for checking the URL launch
 	@Parameters("url")
 	public void launch_JustEat(String url)
 	{
 		driver.get(url);
+		Assert.assertEquals(driver.getCurrentUrl(),url);
+	}
+	
+	//Test for Accessibility Testing
+	@Test(dependsOnMethods= {"launch_JustEat"})
+	public void check_Accessibility()
+	{
+		JSONObject response=new AXE.Builder(driver, axe_Script).analyze();
+		JSONArray violations=response.getJSONArray("violations");
+		if(violations.length()==0)
+		{
+			Assert.assertTrue(true,"No violations found");
+		}
+		else
+		{
+			AXE.writeResults("AccessibilityResponse", response);
+			Assert.assertTrue(false, AXE.report(violations));
+		}
+	}
+	
+	//Test to verify whether error message is displayed when incorrect post code is entered
+	@Test(dependsOnMethods= {"launch_JustEat"})
+	@Parameters("invalid_postcode")
+	public void check_invalid_postcode(String postcode)
+	{
+		driver.findElement(By.name("postcode")).sendKeys(postcode);
+		//Search button click
+		driver.findElement(By.cssSelector("button[data-test-id='find-restaurants-button']")).click();
+		if(driver.findElement(By.id("errorMessage")).isDisplayed())
+			Assert.assertTrue(true,"Error message not displayed when invalid postcode is entered ");
 	}
 	
 	//Test for Searching restaurants on providing a post code
@@ -44,6 +81,7 @@ public class Automation
 	public void search_restaurants_postalcode(String postcode)
 	{
 	ArrayList<String> my_Area_Rests =new ArrayList<String>();
+	driver.findElement(By.name("postcode")).clear();
 	//Input the post code to be searched
 	driver.findElement(By.name("postcode")).sendKeys(postcode);
 	//Search button click
@@ -82,7 +120,7 @@ public class Automation
 	   }		   
    }
    //To fail the test if there is no matching restaurants
-   Assert.assertTrue(my_Area_Rests.size()>0,"No restaurants found matching the entered post code!");
+   Assert.assertTrue(my_Area_Rests.size()>0,"No restaurants retrieved with entered post code.");
    //Logic to print all retrieved restaurants
    System.out.println("List of restaurants with postal code:"+postcode);
    for(int i=0;i<my_Area_Rests.size();i++)
